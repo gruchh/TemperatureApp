07cd8f50d92e3828bb28acd25cd52a5d8fc22281
gruchh <kubol2@gmail.com>
2025-04-02 18:54:01 +0200

    Initial commit

---
+++ src/main/java/com/example/TemperatureApp/TemperatureAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.TemperatureApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemperatureAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemperatureAppApplication.class, args);
+	}
+
+}
